6ed50b8b7e6d1f8b607dc9acd0cc4eb84cd8053c
pmos <movses.saponjyan@gmail.com>
2015-11-01 20:40:58 +0400

    refined slideshow slideshow circles added

--- carousel-slider/carousel.js
+++ carousel-slider/carousel.js
@@ -3,6 +3,7 @@ var images =  ["1.jpg", "2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"];
 function setImage(image) {
 	var slider = document.getElementById("slider");
 	var imagePath = "images" + "/" + image;
+	var images = document.getElementsByClassName("image");
 	while (slider.firstChild) {
 	    slider.removeChild(slider.firstChild);
 	}
@@ -12,15 +13,52 @@ function setImage(image) {
 	slider.appendChild(img);
 }
 
-function slideshow() {
+function drawSlider() {
+	drawCircles();
+	slideShow();
+}
+
+
+function markCircle(index) {
+	var items = document.getElementsByClassName("item");
+      //var backgroundColor = getComputedStyle(items[index]).getPropertyValue("background-color");
+	var backgroundColor = items[index].style.backgroundColor;
+	for (var i = 0; i < items.length; i++) {
+		if (i !== index) {
+			items[i].style.backgroundColor = backgroundColor;
+		} else {
+			items[i].style.backgroundColor =  "green";
+		}
+	}
+}
+
+function slideShow() {
 	var i = 0;
 	window.setInterval(function() {				
 		if (i === images.length) {
 			i = 0;
 		} 
 		setImage(images[i]);
+		markCircle(i);
 		i++;
-	}, 2000);
+	}, 5000);
 }
 
-window.onload = slideshow;
+function drawCircles() {
+	var circle = document.getElementById("circle");
+	while (circle.firstChild) {
+	    circle.removeChild(circle.firstChild);
+	}
+	for (var i = 0; i < images.length; i++) {
+		var div = document.createElement("div");
+		div.className = "item";
+		circle.appendChild(div);
+	}
+}
+
+function gotoSlide(event) {
+	alert(event);
+}
+
+//document.getElementById('circle').onClick = gotoSlide;
+window.onload = drawSlider;
