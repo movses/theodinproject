4a0b6c9864321487709b6dc09b276c6a6bf97520
pmos <movses.saponjyan@gmail.com>
2015-11-08 15:34:43 +0400

    full verions

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,21 +1,29 @@
 $(document).ready( function() {
-	var player = 0;
+	var player = 1;
 
 	var cells = [];
-	var cols = 3;
+	var $cols = 3;
+	var $rows = 3
 
 	//init the cells matrix
-	for ( var i = 0; i < cols; i++ ) {
+	for ( var i = 0; i < $cols; i++ ) {
 	    cells[i] = []; 
 	}
 	
+	function getPlayerSymbol(cell) {
+		if (cell === 1) {
+			return "X";
+		} else if (cell === 0) {
+			return "Y";
+		}
+	}
+
 	$("div .cell").click(function () {
-		togglePlayer();
 		if (!$(this).hasClass('xImg') && !$(this).hasClass('yImg')) {
 			var index = $(this).index();
 			var row = Math.floor(index / 3);
 			var col = index % 3;
-			if(player === 1 ) {				
+			if (player === 1) {				
 				$(this).addClass('xImg');
 				cells[row][col] = 1;
 				checkVictory(row, col);
@@ -25,6 +33,7 @@ $(document).ready( function() {
 				checkVictory(row, col);
 			}
 		}
+		togglePlayer();
 	});
 
 	function togglePlayer() {
@@ -34,51 +43,115 @@ $(document).ready( function() {
 	function checkVictory(row, col) {
 		var rowMatch = 0;
 		var colMatch = 0;
-		var diagMatch = 0;
+		var diag1Match = 0;
+		var diag2Match = 0;
 		var allDefined = true;
 
 		// check row
-		for(var i = -2; i < 2; i++) {			
-			if( undefined !== typeof cells[row][col + i] && cells[row][col+i] === cells[row][col]) {
+		for(var i = -2; i < 3; i++) {			
+			if ((col + i) < 0 || (col + i) >= $cols) {
+				continue;
+			}	
+			if (undefined !== typeof cells[row][col + i] && cells[row][col+i] === cells[row][col]) {
 				rowMatch++;
 			}
 		}
 		if (rowMatch === 3) {
-			alert("player " + cells[row][col] + " won!");
+			var playerSymbol = getPlayerSymbol(cells[row][col]); 
+			$('#message').text("Player " + playerSymbol + " won!");
+			if (playerSymbol === "X") {
+				$('#message').css('color', 'green');
+			} else {
+				$('#message').css('color', 'red');
+			}
+			$("div .cell").off('click');
+			return;
 		}
 
 		// check column
-		for(var i = -2; i < 2; i++) {			
-			if( undefined !== typeof cells[row + i][col] && cells[row + i][col] === cells[row][col]) {
+		for(var i = -2; i < 3; i++) {
+			if ((row + i) < 0 || (row + i) >= $rows) {
+				continue;
+			}	
+			if (undefined !== typeof cells[row + i][col] && cells[row + i][col] === cells[row][col]) {
 				colMatch++;
 			}
 		}
 		if (colMatch === 3) {
-			alert("player " + cells[row][col] + " won!");
+			var playerSymbol = getPlayerSymbol(cells[row][col]); 
+			$('#message').text("Player " + playerSymbol + " won!");
+			if (playerSymbol === "X") {
+				$('#message').css('color', 'green');
+			} else {
+				$('#message').css('color', 'red');
+			}
+			$("div .cell").off('click');
+			return;
 		}
 
-		// check diagonal
-		for(var i = -2; i < 2; i++) {			
-			if( undefined !== typeof cells[row + i][col + i] && cells[row + i][col + i] === cells[row][col]) {
-				diagMatch++;
+		// check diagonal 1 
+		for(var i = -2; i < 3; i++) {			
+			if ((row + i) < 0 || (row + i) >= $rows) {
+				continue;
+			}	
+			if ((col + i) < 0 || (col + i) >= $cols) {
+				continue;
+			}	
+			if (undefined !== typeof cells[row + i][col + i] && cells[row + i][col + i] === cells[row][col]) {
+				diag1Match++;
 			}
 		}
-		if (diagMatch === 3) {
-			alert("player " + cells[row][col] + " won!");
+		if (diag1Match === 3) {
+			var playerSymbol = getPlayerSymbol(cells[row][col]); 
+			$('#message').text("Player " + playerSymbol + " won!");
+			if (playerSymbol === "X") {
+				$('#message').css('color', 'green');
+			} else {
+				$('#message').css('color', 'red');
+			}
+			$("div .cell").off('click');
+			return;
+		}
+
+		// check diagonal 2
+		for(var i = -2; i < 3; i++) {
+			if ((row - i) < 0 || (row - i) >= $rows) {
+				continue;
+			}	
+			if ((col + i) < 0 || (col + i) >= $cols) {
+				continue;
+			}	
+			if (undefined !== typeof cells[row - i][col + i] && cells[row - i][col + i] === cells[row][col]) {
+				diag2Match++;
+			}
+		}
+		if (diag2Match === 3) {
+			var playerSymbol = getPlayerSymbol(cells[row][col]); 
+			$('#message').text("Player " + playerSymbol + " won!");
+			if (playerSymbol === "X") {
+				$('#message').css('color', 'green');
+			} else {
+				$('#message').css('color', 'red');
+			}
+			$("div .cell").off('click');
+			return;
 		}
 
 		// check draw
+		outerLoop: 
 		for (var i = 0; i < 3; i++) {
 			for (var j = 0; j < 3; j++) {
 				if (undefined == cells[i][j]) {
 					allDefined = false;
-					break;
+					break outerLoop;
 				}
 			}
 		}
-
 		if (allDefined) {
-			alert ("draw");
+			$('#message').text("Draw");
+			$('#message').css('color', 'gray');
+			$("div .cell").off('click');
+			return;
 		}
 	}
 });
